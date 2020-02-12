bec806add252492b83d1798cfa033cc5e7b59856
arek2491 <arkadiusz.matysiak@gmail.com>
2020-02-12 20:56:22 +0100

    Zadanie: Stworzenie repozytorium i wysłanie kodu

---
+++ src/test/java/com/crud/tasks/TasksApplicationTests.java
@@ -0,0 +1,14 @@
+package com.crud.tasks;
+
+import org.junit.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TasksApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
